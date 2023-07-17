41c0b37d6499105c7c1dffb2b0e1a2f7668f5bd0
CLAUDIA HERNANDEZ <claudiahergre@gmail.com>
2023-07-17 19:24:37 +0200

    nueva sala provisional

--- src/app/pages/salas/nuevasala/nuevasala.component.ts
+++ src/app/pages/salas/nuevasala/nuevasala.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { SalasService } from 'src/app/services/salas.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nuevasala',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./nuevasala.component.css']
 })
 export class NuevasalaComponent {
+
+  formulario: FormGroup
+  salasServices = inject(SalasService)
+  router = inject(Router)
+
+  constructor() {
+    this.formulario = new FormGroup({
+      nombre: new FormControl(),
+      precio: new FormControl(),
+      direccion: new FormControl(),
+      aforo: new FormControl(),
+      disponible: new FormControl(),
+      // descripcion: new FormControl(),
+      // foto: new FormControl(),
+    })
+  }
+
+  async onSubmit() {
+    // console.log(this.formulario.value)
+    const response = await this.salasServices.create(this.formulario.value)
+    console.log(response)
+
+    if (response.error) {
+      console.log(response.error) //aqui estoy comprobando qué errores me devuelve la aplicacion.
+
+    }
+
+    this.router.navigate(['/salas']) //para que me redirija a esa pagina cuando agregue el nuevo cliente
+  }
+
 
 }
