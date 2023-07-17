aef9c9f851b8baf89f3dd368dc7548e08a3a1b3a
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-07-17 15:37:37 +0200

    estructura inicial, rutas, componentes y servicios generados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { StaffComponent } from './pages/staff/staff.component';
+import { SalasComponent } from './pages/salas/salas.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'usuarios', component: UsuariosComponent },
+  { path: 'staff', component: StaffComponent },
+  { path: 'salas', component: SalasComponent },
+  { path: 'contacto', component: ContactoComponent },
+  { path: '**', redirectTo: '/home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './pages/home/home.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { StaffComponent } from './pages/staff/staff.component';
+import { SalasComponent } from './pages/salas/salas.component';
+import { ContactoComponent } from './pages/contacto/contacto.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    UsuariosComponent,
+    StaffComponent,
+    SalasComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
