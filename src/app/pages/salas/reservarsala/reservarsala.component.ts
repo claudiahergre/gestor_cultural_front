fa52140edc0d8316b31272e6fd609492c5ac931a
CLAUDIA HERNANDEZ <claudiahergre@gmail.com>
2023-07-18 12:31:49 +0200

    formulario de reserva de sala

--- src/app/pages/salas/reservarsala/reservarsala.component.ts
+++ src/app/pages/salas/reservarsala/reservarsala.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-reservarsala',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./reservarsala.component.css']
 })
 export class ReservarsalaComponent {
+
+  formulario: FormGroup
+
+
+  constructor() {
+    this.formulario = new FormGroup({
+      fecha_reserva: new FormControl(),
+      hora_reserva: new FormControl(),
+
+      fecha_fin: new FormControl(),
+      hora_fin: new FormControl(),
+
+      datetime_inicio: new FormControl(),
+      datetime_fin: new FormControl()
+    })
+  }
+
+  onSubmit() {
+    // console.log(this.formulario.value)
+    // 1º concatenar fecha y hora
+    this.formulario.value.datetime_inicio = this.formulario.value.fecha_reserva + ' ' + this.formulario.value.hora_reserva;
+    this.formulario.value.datetime_fin = this.formulario.value.fecha_fin + ' ' + this.formulario.value.hora_fin;
+    // console.log(this.formulario.value.datetime_inicio, this.formulario.value.datetime_fin)
+  }
 
 }
--- src/app/services/salas.service.ts
+++ src/app/services/salas.service.ts
@@ -47,6 +47,8 @@ export class SalasService {
         )
     }
 
+
+
 }
 
 
