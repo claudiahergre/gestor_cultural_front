4b40bf55f71ea52901c10428e082fd3a16dccb81
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-07-20 11:16:47 +0200

    develop solucionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { ContactoComponent } from './pages/contacto/contacto.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
-import { CalendarComponent } from './components/calendar/calendar.component';
 
 
 @NgModule({
@@ -24,14 +23,12 @@ import { CalendarComponent } from './components/calendar/calendar.component';
     StaffComponent,
     SalasComponent,
     ContactoComponent,
-    CalendarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule,
-
+    HttpClientModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
