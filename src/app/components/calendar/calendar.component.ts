9ab62c49acfda8791179ce498487e670c6df39ca
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-07-23 23:19:01 +0200

    getEvents solucionada, @Input reservas: CalendarEvent[] e incluido en el events de fullcalendar

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 // import { CalendarOptions } from '@fullcalendar/core'; // useful for typechecking
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
@@ -6,6 +6,7 @@ import interactionPlugin from '@fullcalendar/interaction';
 import esLocale from '@fullcalendar/core/locales/es';
 import { CalendarService } from 'src/app/services/calendar.service';
 import { Reserva } from 'src/app/interfaces/reserva.interface';
+import { CalendarEvent } from 'src/app/interfaces/calendar_event.interface';
 
 @Component({
   selector: 'app-calendar',
@@ -14,8 +15,7 @@ import { Reserva } from 'src/app/interfaces/reserva.interface';
 })
 export class CalendarComponent implements OnInit {
   calendarServices = inject(CalendarService);
-  
-  public events!: any[];
+
   public options: any;
   public baseUrl: string;
 
@@ -23,6 +23,8 @@ export class CalendarComponent implements OnInit {
   eventDescripcion: string = '';
   eventFechaInicio: string = '';
   eventoFechaFin: string = '';
+
+  @Input() reservas: CalendarEvent[] = [];
 
   constructor() {
     this.baseUrl = 'http://localhost:3000/api/reservas';
@@ -41,61 +43,43 @@ export class CalendarComponent implements OnInit {
       },
       editable: true,
       droppable: true,
-      events: [
-        {
-          title: 'Conferencia',
-          start: new Date().getTime(),
-          description: 'evento 1',
-        },
-        {
-          title: 'Concierto',
-          start: new Date(new Date().getTime() + 86400000),
-          description: 'evento 2',
-        },
-        {
-          title: 'Curso de formación',
-          start: new Date(new Date().getTime() + 86400000 * 2),
-          end: new Date(new Date().getTime() + 86400000 * 3),
-          description: 'evento 2',
-        },
-      ],
+      events: this.reservas,
     };
 
-    this.events = [
-      {
-        title: 'Conferencia',
-        start: new Date().getTime(),
-        description: 'evento 1',
-      },
-      {
-        title: 'Concierto',
-        start: new Date(new Date().getTime() + 86400000),
-        description: 'evento 2',
-      },
-      {
-        title: 'Curso de formación',
-        start: new Date(new Date().getTime() + 86400000 * 2),
-        end: new Date(new Date().getTime() + 86400000 * 3),
-        description: 'evento 2',
-      },
-      {},
-    ];
+    // this.events = [
+    //   {
+    //     title: 'Conferencia',
+    //     start: new Date().getTime(),
+    //     description: 'evento 1',
+    //   },
+    //   {
+    //     title: 'Concierto',
+    //     start: new Date(new Date().getTime() + 86400000),
+    //     description: 'evento 2',
+    //   },
+    //   {
+    //     title: 'Curso de formación',
+    //     start: new Date(new Date().getTime() + 86400000 * 2),
+    //     end: new Date(new Date().getTime() + 86400000 * 3),
+    //     description: 'evento 2',
+    //   },
+    //   {},
+    // ];
   }
 
-  async getEvents(): Promise<Reserva[]> {
+  async getEvents(): Promise<CalendarEvent[] | any>{
     try {
-      const response = await this.calendarServices.getAll<Reserva[]>(
-        this.baseUrl
-      );
-      return response.map((event) => {
+      const response = await this.calendarServices.getAll()
+      const calendarEvents : CalendarEvent[] = response.map((event) => {
         return {
-          title: this.eventTitulo,
-          description: this.eventDescripcion,
-          start: this.eventFechaInicio,
-          end: this.eventoFechaFin,
+          title: event.titulo,
+          description: event.descripcion,
+          start: event.fecha_reserva,
+          end: event.fecha_fin_reserva,
         };
         console.log(event);
       });
+      return calendarEvents;
     } catch (error) {
       console.log(error)
     }
