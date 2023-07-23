327e7fc7460fd278f945c8742c402d3281b5c37a
Gustavo Maldonado <gusjmaldonado@gmail.com>
2023-07-23 13:30:44 +0200

    añerta creada en los registros de usuario y staff

--- src/app/pages/staff/registro/registro-staff.component.ts
+++ src/app/pages/staff/registro/registro-staff.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { StaffService } from 'src/app/services/staff.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-registro-staff',
@@ -16,20 +17,32 @@ export class RegistroStaffComponent {
 
   constructor(private router: Router) {
     this.formulario = new FormGroup({
-      nombre: new FormControl(null,[ Validators.required]),
-      usuario: new FormControl(null,[ Validators.required]),
+      nombre: new FormControl(null, [Validators.required]),
+      usuario: new FormControl(null, [Validators.required]),
       email: new FormControl(null, [
         Validators.required,
         Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,10}$/),
       ]),
-      password: new FormControl(null,[Validators.required]),
-      rol: new FormControl(null,[Validators.required]),
+      password: new FormControl(null, [Validators.required]),
+      rol: new FormControl(null, [Validators.required]),
     });
   }
 
   async onSubmit() {
     const response = await this.staffService.registro(this.formulario.value);
     console.log(response);
+
+    Swal.fire({
+      icon: 'success',
+      title: 'Registro correcto',
+      showConfirmButton: false,
+      timer: 2500,
+      width: 500,
+      padding: '3em',
+      color: '#333333',
+      background: '#0077B6'
+    })
+
 
     if (response.id) {
       this.router.navigate(['/loginStaff'])
