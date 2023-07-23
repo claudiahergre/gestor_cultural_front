795c4dc8dbbdd9265c4719536a8369c0bacc15fe
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-07-23 17:10:10 +0200

    añadidos titulo y descripcion a la interfaz de reserva

--- src/app/interfaces/reserva.interface.ts
+++ src/app/interfaces/reserva.interface.ts
@@ -2,8 +2,11 @@ export interface Reserva {
     id_reserva: number,
     usuarios_id: number,
     salas_id: number,
+    titulo: string,
+    descripcion: string,
     fecha_reserva: string,
     hora_reserva: string,
     fecha_fin_reserva: string,
     hora_fin_reserva: string,
+
 }
