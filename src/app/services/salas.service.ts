80de747d9d077104e88189b73003903197e0a9df
ANA CACERES <acaceresrueda@gmail.com>
2023-07-23 11:17:06 +0200

    rellenado del formulario de editar salas

--- src/app/services/salas.service.ts
+++ src/app/services/salas.service.ts
@@ -1,8 +1,9 @@
 import { Injectable, inject } from '@angular/core';
 import { Sala } from '../interfaces/sala.interface'
 import { Reserva } from '../interfaces/reserva.interface'
-import { firstValueFrom } from 'rxjs';
+import { Observable, firstValueFrom } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 
 @Injectable({
@@ -13,10 +14,14 @@ export class SalasService {
     private httpClient = inject(HttpClient)
     private baseUrl: string
     private usuariosHasSalasUrl: string
+    private mapsUrl = 'https://maps.googleapis.com/maps/api/geocode/json'
+
+
 
     constructor() {
         this.baseUrl = 'http://localhost:3000/api/salas'
         this.usuariosHasSalasUrl = 'http://localhost:3000/api/usuarios_has_salas'
+
     }
 
     getAll(): Promise<Sala[]> {
@@ -41,9 +46,9 @@ export class SalasService {
         }
         //////////
 
-
+        console.log(idSala)
         return firstValueFrom(
-            this.httpClient.get<Sala>(`${this.baseUrl}/editar/${idSala}`, formValue)
+            this.httpClient.get<Sala>(`${this.baseUrl}/${idSala}`)
         )
     }
 
@@ -101,8 +106,10 @@ export class SalasService {
         )
     }
 
-}
 
+
+
+}
 
 
 
