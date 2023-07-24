24332c8606c922815004357ec2b5f5747cdf4bbe
Gustavo Maldonado <gusjmaldonado@gmail.com>
2023-07-24 10:58:42 +0200

    disables

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { StaffService } from 'src/app/services/staff.service';
+import { UsuariosService } from 'src/app/services/usuarios.service';
 
 @Component({
   selector: 'app-menu',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent {
+
+  staffService = inject(StaffService)
+
+  usuariosService = inject(UsuariosService)
+
+
 
 }
