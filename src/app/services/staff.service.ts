94f62a684fc91dc91dd8b24c2179939e6e5c3908
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-07-26 10:32:09 +0200

    nav-bar corregido para panel de control staff

--- src/app/services/staff.service.ts
+++ src/app/services/staff.service.ts
@@ -5,10 +5,9 @@ import { Staff } from '../interfaces/staff.interface';
 import jwtDecode from 'jwt-decode';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StaffService {
-
   private baseUrl: string;
 
   private httpClient = inject(HttpClient);
@@ -18,26 +17,21 @@ export class StaffService {
   }
 
   getAllStaff(): Promise<Staff[]> {
-    return firstValueFrom(
-      this.httpClient.get<Staff[]>(this.baseUrl)
-    );
+    return firstValueFrom(this.httpClient.get<Staff[]>(this.baseUrl));
   }
 
   getById(staffId: number): Promise<Staff | any> {
     return firstValueFrom(
       this.httpClient.get<Staff>(`${this.baseUrl}/id/${staffId}`)
-    )
+    );
   }
 
   //Para obtener del back el perfil del staff logado
   perfil(): Promise<any> {
-    return firstValueFrom(
-      this.httpClient.get<Staff>(`${this.baseUrl}/perfil`)
-    )
+    return firstValueFrom(this.httpClient.get<Staff>(`${this.baseUrl}/perfil`));
   }
 
   registro(formValues: any): Promise<Staff> {
-
     return firstValueFrom(
       this.httpClient.post<Staff>(`${this.baseUrl}/registro`, formValues)
     );
@@ -49,35 +43,34 @@ export class StaffService {
     );
   }
 
-
   updateById(staffId: any, formValues: any): Promise<any> {
     return firstValueFrom(
-      this.httpClient.put<Staff>(`${this.baseUrl}/editar/${staffId}`, formValues)
-    )
+      this.httpClient.put<Staff>(
+        `${this.baseUrl}/editar/${staffId}`,
+        formValues
+      )
+    );
   }
 
   //Para eliminar el staff de la BBDD
   remove(staffId: any): Promise<any> {
-      return firstValueFrom(
+    return firstValueFrom(
       this.httpClient.delete<Staff>(`${this.baseUrl}/${staffId}`)
-    )
+    );
   }
 
-  isLogged(): boolean {
+  isLogged(rol: string = 'staff'): boolean {
     if (localStorage.getItem('token_front')) {
-      const tokenStaff = localStorage.getItem('token_front')
-      const obj = jwtDecode(tokenStaff!) as any
-      console.log(obj)
-      if (obj.userRole) {
-        return true
+      const tokenStaff = localStorage.getItem('token_front');
+      const obj = jwtDecode(tokenStaff!) as any;
+      console.log(obj);
+      if ((obj.userRole && obj.userRole == rol) || rol == 'staff') {
+        return true;
       } else {
-        return false
+        return false;
       }
     } else {
-      return false
+      return false;
     }
   }
-
-
-
 }
